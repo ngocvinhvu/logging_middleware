4d306c585def0d3f69ffb23ea94b838568414d31
vinhvn <vungocvinh95@gmail.com>
2021-08-06 09:19:40 +0700

    fix/readme

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from distutils.core import setup
-import setuptools
 
 setup(
     name="FlaskRequestLogging", # Replace with your own username
     packages = ['FlaskRequestLogging'],
-    version="0.0.2",
+    version="0.1",
     license='MIT',
     description="A middleware to log all information of header and payload of response and request",
     author="vinhvungoc",
     author_email="vinhvungoc@vccorp.vn",
     url="https://github.com/ngocvinhvu/logging_middleware.git",
-    download_url="",
+    download_url="https://github.com/ngocvinhvu/logging_middleware/archive/refs/tags/v0.1.tar.gz",
     keywords=["LOGGING", "FLASK", "PYTHON"],
     install_requires=[
         "logging",
@@ -18,9 +17,9 @@ setup(
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: MIT License',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.8',
     ],
 )
