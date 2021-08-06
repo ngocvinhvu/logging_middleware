d3112770e9ba3f976559ef5f5c6bdeae8856f876
vinhvn <vungocvinh95@gmail.com>
2021-08-06 08:53:44 +0700

    publish to pypi

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
+from distutils.core import setup
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
+setup(
     name="FlaskRequestLogging", # Replace with your own username
+    packages = ['FlaskRequestLogging'],
     version="0.0.2",
-    author="Rose",
+    license='MIT',
+    description="A middleware to log all information of header and payload of response and request",
+    author="vinhvungoc",
     author_email="vinhvungoc@vccorp.vn",
-    description="A middleware to logging request and response",
-    long_description=long_description,
-    long_description_content_type="",
     url="https://github.com/ngocvinhvu/logging_middleware.git",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    download_url="",
+    keywords=["LOGGING", "FLASK", "PYTHON"],
+    install_requires=[
+        "logging",
+        "flask",
     ],
-    python_requires='>=3.8',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
